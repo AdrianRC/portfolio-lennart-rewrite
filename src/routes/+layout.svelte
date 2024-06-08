4bf2433a8c904ec7cf331331bda8ff3282fccd40
AdrianRC <adrn@hey.com>
2024-06-08 13:31:09 -0400

    setup base theme

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+	import '$lib/styles/app.pcss';
+
+	export let data: LayoutData;
+</script>
+
+<div>
+	<!-- <Header collections={data.collections} /> -->
+	<h1>hellooo</h1>
+	<main>
+		<slot />
+	</main>
+	<!-- <Footer payment_methods={data.payment_methods} link_groups={data.footer_links} /> -->
+</div>
